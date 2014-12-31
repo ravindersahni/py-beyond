9508fedeb16da391344e15644abff5eed24532b8
instancetype <ravinderssahni@gmail.com>
2014-12-31 17:38:09 -0500

    Create Reader class

---
+++ organization/reader/reader.py
@@ -0,0 +1,13 @@
+__author__ = 'instancetype'
+
+
+class Reader:
+    def __init__(self, filename):
+        self.filename = filename
+        self.f = open(self.filename, 'rt')
+
+    def close(self):
+        self.f.close()
+
+    def read(self):
+        return self.f.read()
