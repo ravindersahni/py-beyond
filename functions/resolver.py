93e125b4a31d86b71debe7baed71b5d5edd5590f
instancetype <ravinderssahni@gmail.com>
2014-12-31 18:44:28 -0500

    Add clear and has_host methods to Resolver

--- functions/resolver.py
+++ functions/resolver.py
@@ -11,3 +11,9 @@ class Resolver:
         if host not in self._cache:
             self._cache[host] = socket.gethostbyname(host)
         return self._cache[host]
+
+    def clear(self):
+        self._cache.clear()
+
+    def has_host(self, host):
+        return host in self._cache
