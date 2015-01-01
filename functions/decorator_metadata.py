1b8710be78dd66d6159c71b7f68e52d622f4fbd5
instancetype <ravinderssahni@gmail.com>
2015-01-01 11:42:07 -0500

    Use functools.wraps

--- functions/decorator_metadata.py
+++ functions/decorator_metadata.py
@@ -1,5 +1,7 @@
 __author__ = 'instancetype'
 
+import functools
+
 def noop(fn):
     def wrapper():
         return fn()
@@ -12,3 +14,14 @@ def noop(fn):
 def hi():
     """Greet the world from stdout."""
     print('Hello, world.')
+
+def cool_noop(fn):
+    @functools.wraps(fn)
+    def wrapper():
+        return fn()
+    return wrapper
+
+@cool_noop
+def hello():
+    """Greet the world from stdout."""
+    print('Power to the people.')
