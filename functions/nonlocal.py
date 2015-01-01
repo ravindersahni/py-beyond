d060938ae95be835746eb7e1e927b3aa2c386de0
instancetype <ravinderssahni@gmail.com>
2015-01-01 03:11:05 -0500

    Add closure example using nonlocal

---
+++ functions/nonlocal.py
@@ -0,0 +1,18 @@
+__author__ = 'instancetype'
+
+import time
+
+def make_timer():
+    last_called = None
+
+    def elapsed():
+        nonlocal last_called
+        now = time.time()
+        if last_called is None:
+            last_called = now
+            return None
+        result = now - last_called
+        last_called = now
+        return result
+
+    return elapsed
