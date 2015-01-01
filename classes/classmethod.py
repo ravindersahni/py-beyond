0f16d1dfcd48a10b1625e85690b55967e9e1c712
instancetype <ravinderssahni@gmail.com>
2015-01-01 13:24:24 -0500

    Refactor to use classmethod

---
+++ classes/classmethod.py
@@ -0,0 +1,17 @@
+__author__ = 'instancetype'
+
+
+class ShippingContainer:
+
+    next_serial = 0
+
+    @classmethod
+    def _get_next_serial(cls):
+        result = cls.next_serial
+        cls.next_serial += 1
+        return result
+
+    def __init__(self, owner_code, contents):
+        self.owner_code = owner_code
+        self.contents = contents
+        self.serial = ShippingContainer._get_next_serial()
