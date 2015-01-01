c209780fb13085db51fbb4dbecb5814b86e292f1
instancetype <ravinderssahni@gmail.com>
2015-01-01 13:54:29 -0500

    Refactor class to use bic code

--- classes/classmethod.py
+++ classes/classmethod.py
@@ -1,9 +1,14 @@
 __author__ = 'instancetype'
-
+import iso6346
 
 class ShippingContainer:
 
     next_serial = 0
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(owner_code=owner_code,
+                              serial=str(serial).zfill(6))
 
     @classmethod
     def _get_next_serial(cls):
@@ -20,6 +25,7 @@ class ShippingContainer:
         return cls(owner_code, contents=list(items))
 
     def __init__(self, owner_code, contents):
-        self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer._get_next_serial()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code=owner_code,
+            serial=ShippingContainer._get_next_serial())
