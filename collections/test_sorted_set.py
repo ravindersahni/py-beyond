91106a6268a3d5b09f3c6212e80d3dd497cac850
instancetype <ravinderssahni@gmail.com>
2015-01-03 13:35:41 -0500

    Add tests for Equality Protocol

--- collections/test_sorted_set.py
+++ collections/test_sorted_set.py
@@ -144,10 +144,10 @@ class TestReprProtocol(unittest.TestCase):
 class TestEqualityProtocol(unittest.TestCase):
 
     def test_positive_equal(self):
-        self.assertTrue(SortedSet([2, 3, 4]) == SortedSet(2, 3, 4))
+        self.assertTrue(SortedSet([2, 3, 4]) == SortedSet([2, 3, 4]))
 
     def test_negative_equal(self):
-        self.assertFalse(SortedSet([1, 2, 3]) == SortedSet(4, 5, 6))
+        self.assertFalse(SortedSet([1, 2, 3]) == SortedSet([4, 5, 6]))
 
     def test_type_mismatch(self):
         self.assertFalse(SortedSet([1, 2, 3]) == [1, 2, 3])
