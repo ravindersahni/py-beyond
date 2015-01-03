4a393dc56a8d8fab019e69908eab1a958c95cffc
instancetype <ravinderssahni@gmail.com>
2015-01-03 12:13:05 -0500

    Implement __contains__

--- collections/sorted_set.py
+++ collections/sorted_set.py
@@ -4,3 +4,6 @@ __author__ = 'instancetype'
 class SortedSet:
     def __init__(self, items=None):
         self._items = sorted(items) if items is not None else []
+
+    def __contains__(self, item):
+        return item in self._items
