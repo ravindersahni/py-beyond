02b62253d8c4cba35660f361357a4f5e0bb4d75f
instancetype <ravinderssahni@gmail.com>
2015-01-03 12:10:24 -0500

    Add tests to confirm contents

--- collections/test_sorted_set.py
+++ collections/test_sorted_set.py
@@ -29,3 +29,21 @@ class TestConstruction(unittest.TestCase):
 
     def test_default_empty(self):
         s = SortedSet()
+
+
+class TestContainerProtocol(unittest.TestCase):
+
+    def setUp(self):
+        self.s = SortedSet([5, 6, 2, 8])
+
+    def test_positive_contained(self):
+        self.assertTrue(5 in self.s)
+
+    def test_negative_contained(self):
+        self.assertFalse(7 in self.s)
+
+    def test_positive_not_contained(self):
+        self.assertTrue(9 not in self.s)
+
+    def test_negative_not_contained(self):
+        self.assertFalse(2 not in self.s)
