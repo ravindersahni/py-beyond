077310213f96d610c3bf70ea794772b64c7858c4
instancetype <ravinderssahni@gmail.com>
2015-01-04 17:31:57 -0500

    Use inspect.getdoc for clean documentation

--- introspection/inspector.py
+++ introspection/inspector.py
@@ -1,5 +1,7 @@
 __author__ = 'instancetype'
 
+import inspect
+
 def dump(obj):
     print('Type')
     print('====')
@@ -8,7 +10,7 @@ def dump(obj):
 
     print('Documentation')
     print('=============')
-    # TODO
+    print(inspect.getdoc(obj))
     print()
 
     print('Attributes')
