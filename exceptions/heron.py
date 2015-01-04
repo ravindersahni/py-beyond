bc56c67ec7d89851642afc0503414ea00433e2ea
instancetype <ravinderssahni@gmail.com>
2015-01-04 10:57:02 -0500

    Implement and apply basic TriangleError class

--- exceptions/heron.py
+++ exceptions/heron.py
@@ -3,6 +3,14 @@ __author__ = 'instancetype'
 import math
 
 
+class TriangleError(Exception):
+    pass
+
+
 def triangle_area(a, b, c):
+    sides = sorted((a, b ,c))
+    if sides[2] > sides[0] + sides[1]:
+        raise TriangleError('Illegal triangle')
+
     p = (a + b + c) / 2
     return math.sqrt(p * (p - a) * (p- b) * (p - c))
