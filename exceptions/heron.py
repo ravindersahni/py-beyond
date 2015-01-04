5ff55cb17bae17c2b14ca2f40fd89535966b00cb
instancetype <ravinderssahni@gmail.com>
2015-01-04 11:04:46 -0500

    Implememt full TriangleError class

--- exceptions/heron.py
+++ exceptions/heron.py
@@ -4,13 +4,26 @@ import math
 
 
 class TriangleError(Exception):
-    pass
+
+    def __init__(self, msg, sides):
+        super().__init__(msg)
+        self._sides = tuple(sides)
+
+    @property
+    def sides(self):
+        return self._sides
+
+    def __str__(self):
+        return "'{}' for sides {}".format(self.args[0], self._sides)
+
+    def __repr__(self):
+        return 'TriangleError({!r}, {!r}'.format(self.args[0], self._sides)
 
 
 def triangle_area(a, b, c):
     sides = sorted((a, b ,c))
     if sides[2] > sides[0] + sides[1]:
-        raise TriangleError('Illegal triangle')
+        raise TriangleError('Illegal triangle', sides)
 
     p = (a + b + c) / 2
     return math.sqrt(p * (p - a) * (p- b) * (p - c))
