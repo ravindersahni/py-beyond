6c8c2422004121fea5759a0dd3c9a88fb59b41a6
instancetype <ravinderssahni@gmail.com>
2015-01-04 13:34:00 -0500

    Add main to inspect __traceback__

--- exceptions/explicit_chaining.py
+++ exceptions/explicit_chaining.py
@@ -11,3 +11,15 @@ def inclination(dx, dy):
         return math.degrees(math.atan(dy / dx))
     except ZeroDivisionError as e:
         raise InclinationError('Slope cannot be vertical') from e
+
+
+def main():
+    try:
+        inclination(0, 5)
+    except InclinationError as e:
+        print(e.__traceback__)
+
+
+if __name__ == '__main__':
+    main()
+    print('Finished')
