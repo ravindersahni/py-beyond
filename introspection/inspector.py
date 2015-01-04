855335a4b16c35a9d18998d39562c39c5f65b012
instancetype <ravinderssahni@gmail.com>
2015-01-04 17:30:43 -0500

    Begin dump method

---
+++ introspection/inspector.py
@@ -0,0 +1,22 @@
+__author__ = 'instancetype'
+
+def dump(obj):
+    print('Type')
+    print('====')
+    print(type(obj))
+    print()
+
+    print('Documentation')
+    print('=============')
+    # TODO
+    print()
+
+    print('Attributes')
+    print('==========')
+    # TODO
+    print()
+
+    print('Methods')
+    print('=======')
+    # TODO
+    print()
