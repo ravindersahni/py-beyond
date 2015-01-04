37e394fa99396b1a7c4182576f90bdff80c9485e
instancetype <ravinderssahni@gmail.com>
2015-01-04 16:25:48 -0500

    Create logging context manager with contextlib decorator

---
+++ context_managers/decorator.py
@@ -0,0 +1,16 @@
+__author__ = 'instancetype'
+
+import sys
+import contextlib
+
+
+@contextlib.contextmanager
+def logging_context_manager():
+    print('logging_context_manager: Enter')
+
+    try:
+        yield 'Inside the with-block...'
+        print('logging_context_manager: Normal Exit')
+    except Exception:
+        print('logging_context_manager: Exceptional Exit', sys.exc_info())
+
